7d0208b438f96945d023cc607c09a4513d614b6b
Gabriel Movila <gabriel.movila@fortech.ro>
2022-03-10 16:52:41 +0200

    Added dockerfile + dockercompose / Added 2 endpoints (/api and /api2). TODO: solve unit tests

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
-# nodejs-playground
+# Nodejs-playground
 
-## Build and run
+### Prerequisites:
+* Node
 
-</br>
-To build and run the application, you first need to have *Node* installed locally on your computer. You can check if it is installed running the command 'node --version'.
-</br>
-The second step is to run the application by running the 'node app.js' command into your terminal.
+### Build and run
 
+To run the application execute the following command:  ```node app.js``` 
+
+### Docker
+
+* _Docker compose_: ```docker compose up -d``` 
+
+* _Buil and run image_:
+
+  * Build: ```docker build --tag node-docker``` ;
+  * Run with port forward: ```docker run --publish 3000:3000 node-docker``` ;
+  
+### Tests 
+Inside package.json a script for jest testing was added so in order to run the tests use the command: ```npm test```
--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-const http = require('http');
+const axios = require("axios");
+const http = require("http");
 
-const server = http.createServer((req,res) => {
-    if(req.url === '/helloworld'){
-        res.write('Hello world');
+const server = http.createServer((req, res) => {
+  if (req.url === "/helloworld") {
+    res.write("Hello world");
+    res.end();
+  }
+
+  if (req.url === "/api") {
+    axios
+      .get("https://www.boredapi.com/api/activity")
+      .then(function (response) {
+        console.log("Entered then on app.js")
+        res.write(response.data.activity);
         res.end();
-    }
+      });
+  }
+
+  if (req.url === "/api2") {
+    getData(res);
+  }
 });
 
-server.listen(3000)
+const getData = async (res) => {
+  try {
+    let response = await axios.get("https://www.boredapi.com/api/activity");
+    res.write(response.data.activity);
+    res.end();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports=server;
 console.log("Server started");
