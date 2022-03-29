bc73c161fd72c5dc29ceed380f6bccc7115bb139
Gabriel Movila <gabriel.movila@fortech.ro>
2022-03-30 01:17:52 +0300

    Refactored code, added React, added CORS on backend

---
+++ backend/README.md
@@ -0,0 +1,29 @@
+# Nodejs-playground
+
+### Prerequisites:
+
+- Node
+
+### Build and run
+
+To run the application execute the following command: `node app.js`
+
+### Docker
+
+- _Docker compose_: `docker compose up -d`
+
+  - _Cleanup_: `docker compose down --rmi all`
+
+- _Buil and run image_:
+
+  - Build: `docker build --tag node-image .`
+  - Run with port forward: `docker run --name node-container -p 3000:3000 node-image`
+  - Cleanup: `docker rm -f node-container && docker image rm node-app-image`
+
+### Tests
+
+Inside package.json a script for jest testing was added so in order to run the tests use the command: `npm test`
+
+### Typescript
+
+The application is written using TypeScript and ESLint with airbnb config. All the dependencies for that are available as dev dependencies on _package.json_.
---
+++ backend/tests/app.test.ts
@@ -0,0 +1,46 @@
+import axios, { AxiosResponse } from 'axios';
+import * as supertest from 'supertest';
+import { expect, jest } from '@jest/globals';
+import app from '../src/app';
+
+const request = supertest(app);
+
+jest.mock('axios');
+describe('/api endpoint tests', () => {
+  let mockedAxios;
+
+  beforeEach(() => {
+    mockedAxios = jest.spyOn(axios, 'get');
+  });
+
+  afterEach(() => {
+    mockedAxios.mockRestore();
+  });
+
+  it('should fetch the mocked message', async () => {
+    const resp: AxiosResponse = {
+      data: { activity: 'Do Something' },
+      status: 200,
+      statusText: 'Ok',
+      headers: {},
+      config: {},
+    };
+    mockedAxios.mockResolvedValueOnce(resp);
+    const actual = await request.get('/api2');
+    expect(actual.body.activity).toEqual(resp.data.activity);
+    expect(mockedAxios).toHaveBeenCalledWith(
+      'https://www.boredapi.com/api/activity'
+    );
+  });
+
+  it('should not fetch anything', async () => {
+    const message = 'Something went wrong';
+    mockedAxios.mockRejectedValueOnce(new Error(message));
+    const actual = await request.get('/api2');
+    expect(actual.text).toEqual(message);
+    expect(actual.statusCode).toEqual(500);
+    expect(mockedAxios).toHaveBeenCalledWith(
+      'https://www.boredapi.com/api/activity'
+    );
+  });
+});
